3f3cac625135b78738550ffaac3bbbb9e6760588
hildazk <hildazakiatunn@gmail.com>
2025-07-06 22:43:07 +0700

    Add variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable = "app_name" {
+    default = "flask-app-terraform"
+}
+
+variable = "container_port" {
+    default = 5000
+}
+
+variable = "desired_count" {
+    default = 1
+}
+
+variable = "aws_account_id" {
+    description = "022499022990"
+}
